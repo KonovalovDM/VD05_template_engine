c32435d00abf3a5e50c268879fa966185bd93a5c
KonovalovDM <kdm232006@gmail.com>
2024-11-17 01:06:10 +0300

    added code and fixt it

--- main.py
+++ main.py
@@ -4,11 +4,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    context = {
+        "caption": 'А.С. Пушкин о Крыме',
+        "poem": ["Прекрасны вы, брега Тавриды:",
+                 "когда вас видишь с корабля",
+                 "при свете утренней Киприды,",
+                 "как вас впервой увидел я;",
+                 "вы мне предстали в блеске брачном:",
+                 "на небе синем и прозрачном сияли груды ваших гор -",
+                 "долин, деревьев, сел узор",
+                 "разостлан был передо мною."
+                 ]
+    }
+    return render_template('home.html',  **context)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    context = {
+        "image_link": "static/images/crimea_2.jpg"  # Размещаем фото Крыма
+    }
+    return render_template('about.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
